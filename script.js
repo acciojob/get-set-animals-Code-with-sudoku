66feb6125184d522c4f361b8928d1596385a7d68
Code-with-sudoku <137153314+Code-with-sudoku@users.noreply.github.com>
2023-12-30 13:41:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 //complete this code
-class Animal {}
-
-class Dog extends Animal {}
-
-class Cat extends Animal {}
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
 Get Set  Animal class Animal {
 constructor(species) {
     this._species = species;
